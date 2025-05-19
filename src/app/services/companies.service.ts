15ecdfe730184d7879e2711fe330e25fef0ec12b
Andry Mendoza <andryjavier05@gmail.com>
2025-05-19 18:25:28 -0400

    Refactor CompaniesService to enhance getAll method with pagination and filtering capabilities

--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@env/environment';
-import { Company } from '@interfaces/company';
+import { CompanyList } from '@interfaces/company';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,8 +10,24 @@ import { Observable } from 'rxjs';
 export class CompaniesService {
   private http = inject(HttpClient);
 
-  getAll(): Observable<Company[]> {
-    return this.http.get<Company[]>(environment.apiUrl + 'company/');
+  getAll(
+    offset = 0,
+    limit = 10,
+    filter?: string,
+    search?: string
+  ): Observable<CompanyList> {
+    let params = new HttpParams().set('offset', offset).set('limit', limit);
+
+    if (filter) {
+      params = params.set('filter', filter);
+    }
+    if (search) {
+      params = params.set('search', search);
+    }
+
+    return this.http.get<CompanyList>(environment.apiUrl + 'company/', {
+      params,
+    });
   }
 
   delete(id: number): Observable<void> {
