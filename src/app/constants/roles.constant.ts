586b37bd79ef7b52939e94e416914d2680ea9daa
Andry Mendoza <andryjavier05@gmail.com>
2025-05-19 18:25:06 -0400

    Add roles constant for user role management

---
+++ src/app/constants/roles.constant.ts
@@ -0,0 +1,20 @@
+import { Options } from '../types/options';
+
+export const ROLES: Options[] = [
+  {
+    name: 'Super Administrador',
+    value: 0,
+  },
+  {
+    name: 'Administrador',
+    value: 1,
+  },
+  {
+    name: 'Gerente',
+    value: 2,
+  },
+  {
+    name: 'Evaluador',
+    value: 3,
+  },
+];
