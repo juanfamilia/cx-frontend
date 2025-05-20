0927c6925a8969952b0760b2d14055d383937364
Andry Mendoza <andryjavier05@gmail.com>
2025-05-20 01:24:54 -0400

    Refactor CompaniesService to include missing imports and improve method organization

--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@env/environment';
-import { CompanyList } from '@interfaces/company';
+import { Company, CompanyCreate, CompanyList } from '@interfaces/company';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -28,6 +28,18 @@ export class CompaniesService {
     return this.http.get<CompanyList>(environment.apiUrl + 'company/', {
       params,
     });
+  }
+
+  create(data: CompanyCreate): Observable<Company> {
+    return this.http.post<Company>(environment.apiUrl + 'company/', data);
+  }
+
+  getOne(id: number): Observable<Company> {
+    return this.http.get<Company>(environment.apiUrl + 'company/' + id);
+  }
+
+  update(data: CompanyCreate, id: number): Observable<Company> {
+    return this.http.put<Company>(environment.apiUrl + 'company/' + id, data);
   }
 
   delete(id: number): Observable<void> {
