98a49e79c01c70fd0e55bba2d3357b91559963fd
Dreez <contacto@dreez.dev>
2025-07-02 11:57:31 -0400

    Fix URL in getOne method of EvaluationAnalysisService for correct API endpoint

--- src/app/services/evaluation-analysis.service.ts
+++ src/app/services/evaluation-analysis.service.ts
@@ -12,7 +12,7 @@ export class EvaluationAnalysisService {
 
   getOne(id: number): Observable<EvaluationAnalysis> {
     return this.http.get<EvaluationAnalysis>(
-      environment.apiUrl + 'evaluations-analysis/' + id
+      environment.apiUrl + 'evaluation-analysis/' + id
     );
   }
 }
